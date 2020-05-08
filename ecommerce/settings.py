5341e2fd5d939eeacabbbe9d2815d603d7c7932e
J-chaudhary <chaudharyjignesh@gmail.com>
2020-05-08 17:15:25 -0400

    smtp configuration

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -129,5 +129,14 @@ MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'jc23678@gmail.com'
+EMAIL_HOST_PASSWORD = 'RadhikaVeer@9408'
+
 if os.getcwd() == '/app':
     debug=False
